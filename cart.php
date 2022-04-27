cc23736537a24bb4c51d44b1ff2252bb2155ac99
R3Rex31 <rishab3110@gmail.com>
2022-04-28 01:38:59 +0530

    V2

--- cart.php
+++ cart.php
@@ -49,7 +49,7 @@ while ($row = mysqli_fetch_array($result)) {
         echo "<tr><td>" . "#" . $row["id"] . "</td><td>" . $row["Name"] . "</td><td>Rs " . $row["Price"] . "</td><td><a href='cart-remove.php?id={$row['id']}' class='remove_item_link'> Remove</a></td></tr>";
     }
     $id = rtrim($id, ", ");
-    echo "<tr><td></td><td>Total</td><td>Rs " . $sum . "</td><td><a href='success.php' class='btn btn-primary'>Confirm Order</a></td></tr>";
+    echo "<tr><td></td><td>Total</td><td>Rs " . $sum . "</td><td></tr>";
     ?>
                             </tbody>
                             <?php
